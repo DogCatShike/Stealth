7d43ea1cbe5585fa0c071df25018e4ea207b0804
DogCat_Shike <dcsk.flzj@gmail.com>
2025-03-20 00:12:10 +0800

    4.4.4 Done
    看animator是正常切换的，但是动画似乎没播出来

---
+++ Assets/Scripts/SimpleLocomotion.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class SimpleLocomotion
+{
+    public float speedDampTime = 0.1f;
+    public float angularSpeedDampTime = 0.7f;
+    public float angleResponseTime = 0.6f;
+    Animator anim;
+    HashIDs hash;
+
+    public SimpleLocomotion(Animator animator, HashIDs hashIDs)
+    {
+        anim = animator;
+        hash = hashIDs;
+    }
+
+    public void Do(float speed, float angle, float dt)
+    {
+        float angularSpeed = angle / angleResponseTime;
+        anim.SetFloat(hash.speedFloat, speed, speedDampTime, dt);
+        anim.SetFloat(hash.angularSpeedFloat, angularSpeed, angularSpeedDampTime, dt);
+    }
+}
